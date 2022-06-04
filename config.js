f7cdf86582d59e7653c33fa94ef52559d7c93289
smriti <smritishomya12@gmail.com>
2022-06-05 01:51:58 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'facebookAuth': {
+      'clientID': '1088249858709919', // your App ID
+      'clientSecret': '774956995832dd9e6279d9aeb8cfff6e', // your App Secret
+      'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+    }
+  }
