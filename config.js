e59b59fa5f69b7ba4cf0e2234ac2e4a0704beeaa
smritisingh2712 <53440874+smritisingh2712@users.noreply.github.com>
2022-06-04 20:33:58 +0000

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'facebookAuth': {
-      'clientID': '1088249858709919', // your App ID
-      'clientSecret': '774956995832dd9e6279d9aeb8cfff6e', // your App Secret
-      'callbackURL': 'http://localhost:3000/auth/facebook/callback'
-    }
-  }
